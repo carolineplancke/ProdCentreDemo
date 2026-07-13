02e261122defd566bdee70b26642c7d8bb45d6ff
carolineplancke <caroline.plancke@greenshield.ca>
2026-07-13 11:48:51 -0300

    Add App component with ProductMatrix

---
+++ app/App.tsx
@@ -0,0 +1,9 @@
+import { ProductMatrix } from "./components/ProductMatrix";
+
+export default function App() {
+  return (
+    <div className="size-full overflow-auto bg-gray-100">
+      <ProductMatrix />
+    </div>
+  );
+}
